a00f38f74a5ed8309b7b7e5c2207ea32a0602fda
PowerX-NOT <pugalg100@gmail.com>
2023-05-01 22:00:21 +0530

    laurel_sprout: fingerprint: Switch to in-kernel handling of changes

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -34,10 +34,6 @@
 #define PARAM_NIT_FOD 1
 #define PARAM_NIT_NONE 0
 
-#define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
-#define DISPPARAM_HBM_FOD_ON "0x1d20FE0"
-#define DISPPARAM_HBM_FOD_OFF "0x20f0F20"
-
 #define FOD_STATUS_PATH "/sys/class/touch/tp_dev/fod_status"
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
@@ -421,13 +417,11 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_ON);
     mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_FOD);                                                
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
     mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_NONE);
     return Void();
 }
